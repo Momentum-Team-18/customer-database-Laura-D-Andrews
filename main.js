14eee0c681691ea3779e377a6bffb7ba7f52e7fe
Laura-D-Andrews <ldandrews88@gmail.com>
2023-04-25 18:33:27 -0400

    added capitalization and formatting

--- main.js
+++ main.js
@@ -1,54 +1,52 @@
-console.log(customers)
+console.log(customers);
 
-let directoryBox = document.querySelector('#directoryBox')
-// let customerBox = document.querySelectorAll('.customerBox')
-
-// let addCustomerBoxDiv = function
-
+let directoryBox = document.querySelector("#directoryBox");
 
 
 for (let customer of customers) {
-    let customerCard = document.createElement('div');
+  let customerCard = document.createElement("div");
+  customerCard.style.border = '1px solid black'
 
-// picture
-    let customerPicture = document.createElement('img')
-    customerPicture.src = customer.picture.large
-    customerCard.appendChild(customerPicture)
+  // picture
+  let customerPicture = document.createElement("img");
+  customerPicture.src = customer.picture.large;
+  customerCard.appendChild(customerPicture);
 
-// name
-    let customerName = document.createElement('h1')
-    customerName.innerText = `${customer.name.first} ${customer.name.last}`
-    customerCard.appendChild(customerName)
-    customerName.classList.add('custName')
+  // name
+  let customerName = document.createElement("h1");
+  customerName.innerText = `${customer.name.first} ${customer.name.last}`;
 
-// email
-    let customerEmail = document.createElement('p')
-    customerEmail.innerText = customer.email // singular thing so don't need template literal
-    customerCard.appendChild(customerEmail); 
-    customerEmail.classList.add('custEmail')
+  let firstSlice = `${customer.name.first.slice(1, 9)}`;
+  let firstCapital =
+    `${customer.name.first.slice(0, 1).toUpperCase()}` + firstSlice;
+  let lastSlice = `${customer.name.last.slice(1, 9)}`;
+  let lastCapital =
+    `${customer.name.last.slice(0, 1).toUpperCase()}` + lastSlice;
+  customerName.innerText = `${firstCapital} ${lastCapital}`;
 
-// address1
-    let customerAddress = document.createElement('p')
-    customerAddress.innerText = `${customer.location.street.number} ${customer.location.street.name} \n ${customer.location.city} ${customer.location.state} ${customer.location.postcode}`
-    customerCard.appendChild(customerAddress); 
-    customerAddress.classList.add('custInfo')
-           
-// DOB
-    let customerDob = document.createElement('p')
-    let formatDob = moment(customer.dob.date).format("MMM Do YY")
-    customerDob.innerText = `DOB: ${formatDob}`
-    customerCard.appendChild(customerDob)
-    customerDob.classList.add('custInfo')
+  customerCard.appendChild(customerName);
+  customerName.classList.add("custName");
 
-// customer since
-    let customerSince = document.createElement('p')
-    let formatSince = moment(customer.registered.date).format("MMM Do YY")
-    customerSince.innerText = `Customer Since: ${formatSince}`
-    customerCard.appendChild(customerSince)
-    customerSince.classList.add('custInfo')
+  // email
+  let customerEmail = document.createElement("p");
+  customerEmail.innerText = customer.email;
+  customerCard.appendChild(customerEmail);
+  customerEmail.classList.add("custEmail");
 
+  // address
+  let customerAddress = document.createElement("p");
+  let formatState = nameToAbbr(customer.location.state);
+  customerAddress.innerText = `${customer.location.street.number} ${customer.location.street.name} \n ${customer.location.city} ${formatState} ${customer.location.postcode}`;
+  customerCard.appendChild(customerAddress);
+  customerAddress.classList.add("custInfo");
 
-    directoryBox.appendChild(customerCard) // append customerCard to directoryBox at end
+  // dates
+  let customerDates = document.createElement("p");
+  let formatDob = moment(customer.dob.date).format("MMM Do YY");
+  let formatSince = moment(customer.registered.date).format("MMM Do YY");
+  customerDates.innerText = `DOB: ${formatDob} \n Customer Since: ${formatSince}`;
+  customerCard.appendChild(customerDates);
+  customerDates.classList.add("custInfo");
+
+  directoryBox.appendChild(customerCard);
 }
-
-// create/declare a variable to represent the formatted date (let etc..)
